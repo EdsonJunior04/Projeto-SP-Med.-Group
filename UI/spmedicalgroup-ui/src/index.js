2a3340f6519b2b0670c6d5db2160826dd9c776a1
unknown <46813334840>
2021-11-10 17:14:26 -0300

    Java Iniciado]

--- UI/spmedicalgroup-ui/src/index.js
+++ UI/spmedicalgroup-ui/src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {  Route,  BrowserRouter as Router, Redirect, Switch} from 'react-router-dom';
 
 import './index.css';
 
-import Home from './pages/Home/App'
+import Home from './pages/Home/App';
 import Consultas from './pages/consultas/Consultas';
+import NotFound from './pages/notFound/NotFound.js';
+
 
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Consultas />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const routing=(
+  <Router>
+    <div>
+      <Switch>
+      <Route exact path="/" component={ Home } />
+      <Route  path="/consultas" component={ Consultas } />
+      <Route  path="/notfound" component={ NotFound } />
+      <Redirect to="/notfound" />
+      </Switch>
+     
+    </div>
+  </Router>
+)
+
+ReactDOM.render(  routing,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
